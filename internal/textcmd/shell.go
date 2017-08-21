e2c90dc23750f9639b42af219daccd346fead953
Eric Barkie <ebarkie@users.noreply.github.com>
2017-08-21 17:43:39 -0400

    go doc

--- internal/textcmd/shell.go
+++ internal/textcmd/shell.go
@@ -2,9 +2,9 @@
 // Use of this source code is governed by the MIT license
 // that can be found in the LICENSE file.
 
+// Package textcmd implements a simple text command shell and
+// executor.
 package textcmd
-
-// Simple text command shell.
 
 import (
 	"errors"
